19e6777aedc9fd21c2f424232fa5a2afcd1bfe5c
Ben Kallaus <bkallaus@Bens-MacBook-Pro-2.local>
2014-10-02 21:33:42 -0500

    adding the xml util

---
+++ BoardGameStrain/app/src/main/java/edu/se329/teddygrahams/boardgamestrain/BoardGame.java
@@ -0,0 +1,88 @@
+package edu.se329.teddygrahams.boardgamestrain;
+
+/**
+ * Created by bkallaus on 9/29/14.
+ */
+public class BoardGame {
+
+    private String name;
+    private int minPlayers;
+    private int maxPlayers;
+    private int playTime;
+    private int ratingValue;
+
+    public BoardGame (String name){
+        this.name = name;
+    }
+
+    public BoardGame(String name, int min, int max, int play){
+        this.name = name;
+        minPlayers = min;
+        maxPlayers = max;
+        playTime = play;
+        ratingValue = 0;
+    }
+
+    public BoardGame(String name, int min, int max, int play, int rating){
+        this.name = name;
+        minPlayers = min;
+        maxPlayers = max;
+        playTime = play;
+        ratingValue = rating;
+    }
+
+    public int getMinPlayers() {
+        return minPlayers;
+    }
+
+    public void setMinPlayers(int minPlayers) {
+        this.minPlayers = minPlayers;
+    }
+
+    public int getPlayTime() {
+        return playTime;
+    }
+
+    public void setPlayTime(int playTime) {
+        this.playTime = playTime;
+    }
+
+    public int getMaxPlayers() {
+        return maxPlayers;
+    }
+
+    public void setMaxPlayers(int maxPlayers) {
+        this.maxPlayers = maxPlayers;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
+
+//<item objecttype="thing" objectid="7865" subtype="boardgame" collid="1108162">
+//<name sortindex="1">10 Days in Africa</name>
+//<yearpublished>2003</yearpublished>
+//<image>http://cf.geekdo-images.com/images/pic1229634.jpg</image>
+//<thumbnail>
+//http://cf.geekdo-images.com/images/pic1229634_t.jpg
+//</thumbnail>
+//<stats minplayers="2" maxplayers="4" playingtime="25" numowned="1707">
+//<rating value="8">
+//<usersrated value="1524"/>
+//<average value="6.57129"/>
+//<bayesaverage value="6.25256"/>
+//<stddev value="1.17318"/>
+//<median value="0"/>
+//</rating>
+//</stats>
+//<status own="1" prevowned="0" fortrade="0" want="0" wanttoplay="0" wanttobuy="0" wishlist="0" preordered="0" lastmodified="2007-10-12 22:51:58"/>
+//<numplays>20</numplays>
+//<comment>
+//Light filler and as a nice side effect you learn some African geography.
+//</comment>
--- BoardGameStrain/app/src/main/java/edu/se329/teddygrahams/boardgamestrain/ResultFragment.java
+++ BoardGameStrain/app/src/main/java/edu/se329/teddygrahams/boardgamestrain/ResultFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 
 /**
@@ -24,5 +25,31 @@ public class ResultFragment extends Fragment {
         Log.e("BackStackers", "BackStack: " + getFragmentManager().getBackStackEntryCount());
         return view;
     }
+class ResultsListAdapter extends BaseAdapter{
 
+    public ResultsListAdapter(String[] list)
+    {
+
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}
 }
