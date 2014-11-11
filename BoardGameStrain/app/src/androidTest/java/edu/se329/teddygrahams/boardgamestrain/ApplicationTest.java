6fbd1e93d95da707e6abbf90d42e6eee87801a72
Ben Kallaus <bkallaus@iastate.edu>
2014-11-10 20:18:35 -0600

    adding test cases

--- BoardGameStrain/app/src/androidTest/java/edu/se329/teddygrahams/boardgamestrain/ApplicationTest.java
+++ BoardGameStrain/app/src/androidTest/java/edu/se329/teddygrahams/boardgamestrain/ApplicationTest.java
@@ -2,6 +2,7 @@ package edu.se329.teddygrahams.boardgamestrain;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
+import android.widget.TextView;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
@@ -10,4 +11,38 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
     }
+
+    public void testBoardGameModel() throws IllegalArgumentException {
+        String name = "Test";
+        int min = 0;
+        int max = 3;
+        int time = 30;
+        BoardGame game = new BoardGame(name,min,max,time);
+        assertEquals(game.getName(), name);
+    }
+    public void testBoardGameModel1() throws IllegalArgumentException {
+        String name = "Test";
+        int min = 0;
+        int max = 3;
+        int time = 30;
+        BoardGame game = new BoardGame(name,min,max,time);
+        assertEquals(game.getMinPlayers(), min);
+    }
+    public void testBoardGameModel2() throws IllegalArgumentException {
+        String name = "Test";
+        int min = 0;
+        int max = 3;
+        int time = 30;
+        BoardGame game = new BoardGame(name,min,max,time);
+        assertEquals(game.getMaxPlayers(), max);
+    }
+    public void testBoardGameModel3() throws IllegalArgumentException {
+        String name = "Test";
+        int min = 0;
+        int max = 3;
+        int time = 30;
+        BoardGame game = new BoardGame(name,min,max,time);
+        assertEquals(game.getPlayTime(), time);
+    }
+
 }
---
+++ BoardGameStrain/app/src/androidTest/java/edu/se329/teddygrahams/boardgamestrain/MainActivityTest.java
@@ -0,0 +1,34 @@
+package edu.se329.teddygrahams.boardgamestrain;
+
+import android.app.Activity;
+import android.app.Application;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.ApplicationTestCase;
+import android.widget.TextView;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Ben on 11/10/2014.
+ */
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    private MainActivity mActivity;
+
+    public MainActivityTest() {
+        super(MainActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        mActivity = getActivity();
+    }
+
+    public void testBackStack(){
+        assertEquals(mActivity.getFragmentManager().getBackStackEntryCount(),0);
+    }
+
+    //insert networking test cases
+
+}
